ea71c3b253055c45e925b8497a35afce79c42fde
toto <ro.marcus@free.fr>
2019-11-09 16:51:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if ( string === string.toLowerCase()) {
-    return ('"I can\'t hear you!" if `string` is lowercase');
+    return ("I can\'t hear you!");
   } else {
 
     return ('"YES INDEED!" if `string` is uppercase');

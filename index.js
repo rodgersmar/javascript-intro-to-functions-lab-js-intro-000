86fcc8d46b60964b3df5a8d1629e22b97b5c8250
toto <ro.marcus@free.fr>
2019-11-09 16:02:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 function shout(string) {
   return string.toUpperCase();
 }
+
 function whisper(string) {
   return string.toLowerCase();
 }
+
 function logShout(string) {
   string = string.toUpperCase();
   console.log(string);
-
 }
+
 function logWhisper(string) {
   string = string.toLowerCase();
   console.log(string);
+}
 
+sayHiToGrandma(string) {
+returns ("I can\'t hear you!" if `string` is lowercase')
+  
 }

e26bfc9a26debf44e41fe8197c53adf5e1e6ea37
toto <ro.marcus@free.fr>
2019-11-09 17:14:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string) {
   else if ( "string === string.toUpperCase") {
     return ("YES INDEED!");
   }
-  if ("string === I love you, Grandma.")
-    return ("I love you, too."); {
+  else if ("string === I love you, Grandma.") {
+    return ("I love you, too."); 
   }
 }
